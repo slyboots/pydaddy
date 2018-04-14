ef833b4e92790ee72c92396fdb47c0ad68e4e051
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-04-14 17:10:08 -0500

    godaddy api client doenst work ;P

--- pydaddy/config.py
+++ pydaddy/config.py
@@ -5,8 +5,8 @@ DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE')
 
 def get_config(url=None,access_key=None,secret_key=None):
     return {
-        'GDAPI_URL': url or os.getenv('GDAPI_URL'),
-        'GDAPI_ACCESS_KEY': access_key or os.getenv('GDAPI_ACCESS_KEY'),
-        'GDAPI_SECRET_KEY': secret_key or os.getenv('GDAPI_SECRET_KEY')
+        'url': url or os.getenv('GDAPI_URL'),
+        'access_key': access_key or os.getenv('GDAPI_ACCESS_KEY'),
+        'secret_key': secret_key or os.getenv('GDAPI_SECRET_KEY')
     }
 
